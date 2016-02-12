284ca7de1d231a5ba6aae8044a72f951a841580d
Alan G. Kiraly <agkiraly@gmail.com>
2016-02-11 22:58:20 -0800

    add path module, filter files by extname, change post to /upload/

--- routes/pdf_parser.js
+++ routes/pdf_parser.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 var parse_pdf = function(file) {
-  var cmd = 'pdf2txt';
+  var cmd = '/usr/bin/pdftotext';
   var args = '';
   exec(cmd, function(error, stdout, sterr) {
   
--- routes/posthandler.js
+++ routes/posthandler.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var posthandler = express.Router();
 var multer = require('multer');
-var upload = multer({dest:'./uploads/'});
+var path = require('path');
+var upload = multer({
+  dest:'./uploads/',
+  fileFilter: function(req, file, cb) {
+    // console.dir(file);
+
+    if (path.extname(file.originalname) !== '.pdf') {
+      return cb(new Error('Only pdfs are allowed'));
+    }
+
+    cb(null,true);
+  }
+});
 
 posthandler.post('/',upload.single('estmt'), function(req, res, next) {
   //req.file is the 'estmt' file
-  //req.body will hold the text fields, if any
-  console.log(req.body);
+  console.dir(req.file);
+  //req.body will hold any text fields
+  //console.log(req.body);
   res.status(204).end();
 });
 
 module.exports = posthandler;
-
-
-//var upload = multer({dest:'./uploads/', fileFilter:function(req, file, cb) {
-  //to reject
-  //cb(null,false);
-  //
-  //to accept
-//  cb(null,true);
-  //
-  //You can always pass an error if something goes wrong
-  //cb(new Error('I don\'t have a clue!'));
-//}
 
