3f4418516a7a2f8938fdfff6f657fdb147a22901
Alan G. Kiraly <agkiraly@gmail.com>
2016-02-18 20:32:08 -0800

    text_parser.js workign from /tmp

--- routes/text_parser.js
+++ routes/text_parser.js
@@ -1,108 +1,105 @@
 var fs = require('fs');
+var text_parser =  function() {
+  this.process = function(opts) {
+    var filepath = opts.filepath,
+        destination = opts.destination,
+        buf;
 
-var fpath = '';
+    if( filepath.length ) {
+      buf = fs.readFile( filepath, 'utf8', this.parse );
+    }
+  };
+  this.parse = function (err,data) {
+    if( ! err ) {
+      var lines,
+          outfile,
+          lenLines,
+          curLine,
+          occurences,
+          preppedStr,
+          parts,
+          i,
+          j;
 
-
-//var outFile = '/Users/alan/Documents/data.qif';
-//var outFile = 'data.qif';
-
-if( process.argv.length > 2 ) {
- fpath = process.argv[2];
- outFile = process.argv[3];
-}
-
-if( fpath.length ) {
- var buf = fs.readFile( fpath, 'utf8', cb );
-}
-
-function cb(err,data) {
-//var outFile = '/Users/alan/Documents/data.qif';
-var outFile = 'data.qif';
- if( ! err ) {
-  var l = splitNL(data);
-  //console.log(l);
-  var l = prepLines(l); 
-  //console.log(l);
-  //return false;
-  var qif = ['!Type:Bank'];
-//  console.log(l.length);
-  for (var i=0; i<l.length; i++) {
-//    console.log(findFirstDate(l[i]));
-    var curLine = l[i];
-   // console.log(curLine);
-    if ( findFirstDate(curLine) ) {
-      var curLine = l[i];
-      curLine = curLine.replace(/(\d{2}\/\d{2}\/\d{2})/g, '|$1 ');
-      curLine = curLine.replace(/\s+/g,' ');
-      var instances = curLine.split('|');
-      for (var j=1; j<instances.length; j++) {
-        var preppedStr = prepInstance(instances[j]);
-        var parts = getParts(preppedStr);
-        var finalStr = '';
-        //console.log(parts);
-        qif.push('D'+parts.D);
-        qif.push('T'+parts.T);
-        qif.push('P'+parts.P);
-        qif.push('^');
-//        console.log(qif);
-//        return false;
-        //qif.push(prepInstance(instances[j]));
+console.log('***********************');
+console.dir(this);
+console.log('***********************');
+lines = this.splitNL(data);
+      // console.log(lines);
+      lines = this.prepLines(lines); 
+      // console.log(lines);
+      lenLines = lines.length;
+      // return false;
+      outfile = ['!Type:Bank'];
+      // console.log(lines.length);
+      for (i=0; i<lenLines; i++) {
+        // console.log(findFirstDate(lines[i]));
+        curLine = lines[i];
+        // console.log(curLine);
+        if ( this.findFirstDate(curLine) ) {
+          curLine = curLine.replace(/(\d{2}\/\d{2}\/\d{2})/g, '|$1 ');
+          curLine = curLine.replace(/\s+/g,' ');
+          occurences = curLine.split('|');
+          for (j=1; j<occurences.length; j++) {
+            preppedStr = this.prepInstance(ocurrences[j]);
+            parts = this.getParts(preppedStr);
+            //console.log(parts);
+            outfile.push('D'+parts.D,'T'+parts.T,'P'+parts.P,'^');
+            // console.log(outfile);
+            // return false;
+          }
+        }
       }
-    } else {
-  //    console.log('no date');
+      // console.log(outfile);
+      fs.writeFile(destination, outfile.join('\n'), function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log(destination + ' saved!');
+      });
     }
-  }
-//  console.log(qif);
-  console.log(outFile);
-  fs.writeFile(outFile, qif.join('\n'), function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-  });
- }
-}
+  };
+  this.reFormatDate = function(d) {
+    var dParts = d.split('/');
+    return '20' + trimStr(dParts[2]) + '-' + dParts[0] + '-' + dParts[1] + '|';
+  };
+  this.getParts = function(str) {
+    var parts = {};
+    var partsAr = str.split('|');
+    parts.D = partsAr[0];
+    parts.P = partsAr[1];
+    parts.T = partsAr[2]; // we might need to replace commas here
+    return parts;
+  };
+  this.prepInstance = function(str) {
+    var datere = /^(\d{2}\/\d{2}\/\d{2})\s?/; 
+    str = str.replace(/^\s+|\s+$/g,'');
+    //str = str.replace(/,/g,' ');
+    str = str.replace(/'/g,'');
+    str = str.replace(/\s?(-?)([\d\.]+)$/,'|$1$2');
+    str = str.replace(datere,reFormatDate);
+    return str;
+  };
+  this.trimStr = function(str) {
+    return str.replace(/^\s+|\s+$/g,'');
+  };
+  this.findFirstDate = function(line) {
+    // var re = new RegExp (/^s+/ + getDateReg());
+    // return line.match(re);
+    return line.match(/^\s?(\d{2}\/\d{2}\/\d{2})/g);
+  };
+  this.splitNL = function(data) {
+    return data.split('\n');
+  };
+  this.prepLines = function(arr) {
+    return arr.filter( 
+      function(str) { 
+        str = trimStr(str); 
+        return str.length;
+      }
+    );
+  };
+  return this;
+};
 
-function reFormatDate(d) {
-  var dParts = d.split('/');
-  return '20' + trimStr(dParts[2]) + '-' + dParts[0] + '-' + dParts[1] + '|';
-}
-
-function getParts(str) {
-  var parts = {};
-  var partsAr = str.split('|');
-  parts.D = partsAr[0];
-  parts.P = partsAr[1];
-  parts.T = partsAr[2]; // we might need to replace commas here
-  return parts;
-}
-
-function prepInstance(str) {
-  var datere = /^(\d{2}\/\d{2}\/\d{2})\s?/; 
-  str = str.replace(/^\s+|\s+$/g,'');
-  //str = str.replace(/,/g,' ');
-  str = str.replace(/'/g,'');
-  str = str.replace(/\s?(-?)([\d\.]+)$/,'|$1$2');
-  str = str.replace(datere,reFormatDate);
-  return str;
-}
-
-function trimStr(str) {
-  return str.replace(/^\s+|\s+$/g,'');
-}
-
-function findFirstDate(line) {
-  //var re = new RegExp (/^s+/ + getDateReg());
-  //return line.match(re);
-  return line.match(/^\s?(\d{2}\/\d{2}\/\d{2})/g);
-}
-
-function splitNL(data) {
-  return data.split('\n');
-}
-function prepLines(arr) {
-  return arr.filter( 
-    function(str) { 
-      str = trimStr(str); 
-      return str.length;
-    }
-  );
-}
+module.exports = new text_parser();
